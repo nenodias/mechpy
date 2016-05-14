8fa79d60c547ff0c11a24879efb8fd5183941769
Horácio Dias <horacio.dias92@gmail.com>
2016-05-13 21:07:11 -0300

    Comitando projeto inicial

---
+++ mechpy/core/models.py
@@ -0,0 +1,99 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+class Pessoa(models.Model):
+
+    TIPO_PESSOA_FISICA = 'PF'
+    TIPO_PESSOA_JURIDICA = 'PJ'
+
+    TIPO_PESSOA = (
+        (TIPO_PESSOA_FISICA, 'Pessoa Física'),
+        (TIPO_PESSOA_JURIDICA, 'Pessoa Jurídica'),
+    )
+
+    nome = models.CharField('Nome',max_length=200, blank=False, null=False)
+    observacao = models.TextField('Observação', blank=True, null=True)
+    telefone = models.CharField('Telefone',max_length=20)
+    celular = models.CharField('Celular',max_length=20)
+    email = models.EmailField('Email')
+    tipo = models.CharField('Tipo',max_length=2, blank=False, null=False, choices=TIPO_PESSOA)
+    pessoa_juridica = models.OneToOneField('PessoaJuridica', verbose_name='Pessoa Jurídica',null=True, blank=True)
+    pessoa_fisica = models.OneToOneField('PessoaFisica', verbose_name='Pessoa Física',null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Pessoa'
+        verbose_name_plural = 'Pessoas'
+
+class PessoaFisica(models.Model):
+    rg = models.CharField('RG',max_length=20)
+    cpf = models.IntegerField('CPF', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Pessoa Física'
+        verbose_name_plural = 'Pessoas Físicas'
+
+class PessoaJuridica(models.Model):
+    cnpj = models.IntegerField('CNPJ', blank=True, null=True)
+    razao_social = models.CharField('Razão Social',max_length=200)
+    nome_fantasia = models.CharField('Nome Fantasia',max_length=200)
+    descricao = models.CharField('Descrição',max_length=200)
+    inscricao_estadual = models.CharField('Inscrição Estadual',max_length=30)
+    inscricao_municipal = models.CharField('Inscrição Municipal',max_length=30)
+
+    class Meta:
+        verbose_name = 'Pessoa Jurídica'
+        verbose_name_plural = 'Pessoas Jurídicas'
+
+class Contato(models.Model):
+    nome = models.CharField('Nome',max_length=20)
+    email = models.EmailField('Email', blank=True, null=True)
+    telefone = models.CharField('Telefone',max_length=20)
+    pessoa = models.ForeignKey('Pessoa', related_name='contatos')
+
+    class Meta:
+        verbose_name = 'Contato'
+        verbose_name_plural = 'Contatos'
+
+class Pais(models.Model):
+
+    nome = models.CharField('Nome',max_length=100, blank=False, null=False)
+    sigla = models.CharField('Sigla',max_length=2, blank=False, null=False)
+    
+    class Meta:
+        verbose_name = 'País'
+        verbose_name_plural = 'Países'
+
+class Estado(models.Model):
+
+    nome = models.CharField('Nome',max_length=100, blank=False, null=False)
+    sigla = models.CharField('Sigla',max_length=2, blank=False, null=False)
+    pais = models.ForeignKey('Pais', related_name='estados')
+    
+    class Meta:
+        verbose_name = 'Estado'
+        verbose_name_plural = 'Estados'
+
+
+class Cidade(models.Model):
+
+    nome = models.CharField('Nome',max_length=100, blank=False, null=False)
+    codigo_ibge = models.CharField('Código IBGE',max_length=30, blank=True, null=True)
+    estado = models.ForeignKey('Estado', related_name='cidades')
+
+    class Meta:
+        verbose_name = 'Cidade'
+        verbose_name_plural = 'Cidades'
+
+class Endereco(models.Model):
+
+    nome = models.CharField('Nome',max_length=100, blank=False, null=False)
+    numero = models.CharField('Número', max_length=50)
+    rua = models.CharField('Rua', max_length=100)
+    bairro = models.CharField('Bairro', max_length=100)
+    cep = models.CharField('Cep', max_length=20)
+    cidade = models.OneToOneField('Cidade', verbose_name='Cidade')
+    pessoa = models.ForeignKey('Pessoa', related_name='enderecos')
+
+    class Meta:
+        verbose_name = 'Endereço'
+        verbose_name_plural = 'Endereços'
---
+++ mechpy/oficina/models.py
@@ -0,0 +1,211 @@
+from django.db import models
+from mechpy.core.models import Pessoa
+
+class PessoaVeiculo(models.Model):
+    STATUS_DONO = "DN"
+    STATUS_ANTIGO = "AT"
+
+    STATUS = (
+        (STATUS_DONO, 'Dono'),
+        (STATUS_ANTIGO, 'Antigo'),
+    )
+    status = models.CharField('Status',max_length=2, choices=STATUS, blank=False, null=False)
+    data = models.DateField('Data', auto_now_add=True, blank=True)
+    ativo = models.BooleanField('Ativo', blank=True, default=True)
+    km = models.IntegerField('KM')
+
+    pessoa = models.ForeignKey(Pessoa, related_name='pessoa_veiculos', null=False, blank=False)
+    veiculo = models.ForeignKey('Veiculo', related_name='pessoa_veiculos', null=False, blank=False)
+
+    class Meta:
+        verbose_name = 'Marca'
+        verbose_name_plural = 'Marcas'
+        unique_together = (('pessoa', 'veiculo'))
+
+
+class Marca(models.Model):
+
+    nome = models.CharField('Nome',max_length=100, blank=False, null=False)
+    descricao = models.CharField('Descrição',max_length=200)
+
+    class Meta:
+        verbose_name = 'Marca'
+        verbose_name_plural = 'Marcas'
+
+class Modelo(models.Model):
+
+    nome = models.CharField('Nome',max_length=100, blank=False, null=False)
+    descricao = models.CharField('Descrição',max_length=200)
+    ano = models.CharField('Ano',max_length=20)
+    marca = models.ForeignKey('Marca', related_name='modelos')
+
+    class Meta:
+        verbose_name = 'Modelo'
+        verbose_name_plural = 'Modelo'
+
+class Categoria(models.Model):
+
+    nome = models.CharField('Nome',max_length=100, blank=False, null=False)
+    descricao = models.CharField('Descrição',max_length=200)
+    produtos = models.ManyToManyField('Produto', related_name = 'categorias')
+
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+
+class Produto(models.Model):
+
+    nome = models.CharField('Nome',max_length=100, blank=False, null=False)
+    descricao = models.CharField('Descrição',max_length=200)
+    valor = models.DecimalField('Valor',max_digits=15, decimal_places=2)
+
+    class Meta:
+        verbose_name = 'Produto'
+        verbose_name_plural = 'Produtos'
+
+class Cor(models.Model):
+
+    nome = models.CharField('Nome',max_length=50, blank=False, null=False)
+
+    class Meta:
+        verbose_name = 'Cor'
+        verbose_name_plural = 'Cores'
+
+class Veiculo(models.Model):
+
+    observacoes = models.TextField('Observações')
+    ano = models.CharField('Ano',max_length=20)
+    cor = models.ForeignKey('Cor', related_name='veiculos')
+    km = models.IntegerField('KM', blank=True, null=True)
+    modelo = models.ForeignKey('Modelo', related_name='veiculos', null=False, blank=False)
+    
+
+    class Meta:
+        verbose_name = 'Produto'
+        verbose_name_plural = 'Produtos'
+
+class Servico(models.Model):
+
+    nome = models.CharField('Nome',max_length=100, blank=False, null=False)
+    descricao = models.CharField('Descrição',max_length=200)
+    valor = models.DecimalField('Valor',max_digits=15, decimal_places=2)
+
+    class Meta:
+        verbose_name = 'Serviço'
+        verbose_name_plural = 'Serviços'
+
+class PedidoVenda(models.Model):
+
+    descricao = models.CharField('Descrição',max_length=200)
+    valor = models.DecimalField('Valor',max_digits=15, decimal_places=2)
+    data_inicio = models.DateField('Data Entrada',auto_now_add=True)
+    data_termino = models.DateField('Data Saída', null=True, blank=True)
+    km = models.IntegerField('KM', blank=True, null=True)
+    pessoa_veiculo = models.ForeignKey('PessoaVeiculo', related_name='pedidos_venda', null=False, blank=False)
+
+    class Meta:
+        verbose_name = 'Pedido Venda'
+        verbose_name_plural = 'Pedidos Venda'
+
+class ItemServicoVenda(models.Model):
+    descricao = models.CharField('Descrição',max_length=200)
+    valor = models.DecimalField('Valor',max_digits=15, decimal_places=2)
+    servico = models.ForeignKey('Servico', related_name='itens_servico_venda', null=False, blank=False)
+    pedido_venda = models.ForeignKey('PedidoVenda', related_name='itens_servico', null=False, blank=False)
+
+    class Meta:
+        verbose_name = 'Item Serviço Venda'
+        verbose_name_plural = 'Itens Serviço Venda'
+
+
+class ItemProdutoVenda(models.Model):
+    descricao = models.CharField('Descrição',max_length=200)
+    valor = models.DecimalField('Valor',max_digits=15, decimal_places=2)
+    produto = models.ForeignKey('Produto', related_name='itens_produto_venda', null=False, blank=False)
+    pedido_venda = models.ForeignKey('PedidoVenda', related_name='itens_produto', null=False, blank=False)
+
+    class Meta:
+        verbose_name = 'Item Produto Venda'
+        verbose_name_plural = 'Itens Produto Venda'
+
+class Funcionario(models.Model):
+
+    nome = models.CharField('Nome',max_length=100, blank=False, null=False)
+    funcao = models.CharField('Função',max_length=100, blank=False, null=False)
+    data_entrada = models.DateField('Data Entrada',auto_now_add=True)
+    data_saida = models.DateField('Data Saída', null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Funcionário'
+        verbose_name_plural = 'Funcionários'
+
+class Apontamento(models.Model):
+    descricao = models.CharField('Descrição',max_length=200, blank=False, null=False)
+    data_inicio = models.DateField('Data Início',auto_now_add=True)
+    data_termino = models.DateField('Data Término', null=True, blank=True)
+
+    pedido_venda = models.ForeignKey('PedidoVenda', related_name='apontamentos', null=False, blank=False)
+    servico = models.ForeignKey('Servico', related_name='apontamentos', null=False, blank=False)
+    funcionario = models.ForeignKey('Funcionario', related_name='apontamentos', null=False, blank=False)
+
+    class Meta:
+        verbose_name = 'Apontamento'
+        verbose_name_plural = 'Apontamentos'
+
+class PedidoCompra(models.Model):
+
+    descricao = models.CharField('Descrição',max_length=200)
+    valor = models.DecimalField('Valor',max_digits=15, decimal_places=2)
+    data_inicio = models.DateField('Data Entrada',auto_now_add=True)
+    data_termino = models.DateField('Data Saída', null=True, blank=True)
+    km = models.IntegerField('KM', blank=True, null=True)
+    pessoa_veiculo = models.ForeignKey('PessoaVeiculo', related_name='pedidos_compra', null=False, blank=False)
+
+    class Meta:
+        verbose_name = 'Pedido Compra'
+        verbose_name_plural = 'Pedidos Compra'
+
+class ItemServicoCompra(models.Model):
+    descricao = models.CharField('Descrição',max_length=200)
+    valor = models.DecimalField('Valor',max_digits=15, decimal_places=2)
+    servico = models.ForeignKey('Servico', related_name='itens_servico_compra', null=False, blank=False)
+    pedido_compra = models.ForeignKey('PedidoCompra', related_name='itens_servico', null=False, blank=False)
+
+    class Meta:
+        verbose_name = 'Item Serviço Compra'
+        verbose_name_plural = 'Itens Serviço Compra'
+
+
+class ItemProdutoCompra(models.Model):
+    descricao = models.CharField('Descrição',max_length=200)
+    valor = models.DecimalField('Valor',max_digits=15, decimal_places=2)
+    produto = models.ForeignKey('Produto', related_name='itens_produto_compra', null=False, blank=False)
+    pedido_compra = models.ForeignKey('PedidoCompra', related_name='itens_produto', null=False, blank=False)
+
+    class Meta:
+        verbose_name = 'Item Produto Compra'
+        verbose_name_plural = 'Itens Produto Compra'
+
+class FaturaRecebimento(models.Model):
+    valor = models.DecimalField('Valor',max_digits=15, decimal_places=2)
+    valor_pago = models.DecimalField('Valor',max_digits=15, decimal_places=2)
+    data_pagamento = models.DateField('Data Pagamento')
+    data_vencimento = models.DateField('Data Vencimento')
+    juros_vencimento = models.DecimalField('Juros Vencimento',max_digits=15, decimal_places=2)
+    pedido_venda = models.ForeignKey('PedidoVenda', related_name='faturas', null=False, blank=False)
+
+    class Meta:
+        verbose_name = 'Fatura Recebimento'
+        verbose_name_plural = 'Faturas Recebimento'
+
+class FaturaPagamento(models.Model):
+    valor = models.DecimalField('Valor',max_digits=15, decimal_places=2)
+    valor_pago = models.DecimalField('Valor',max_digits=15, decimal_places=2)
+    data_pagamento = models.DateField('Data Pagamento')
+    data_vencimento = models.DateField('Data Vencimento')
+    juros_vencimento = models.DecimalField('Juros Vencimento',max_digits=15, decimal_places=2)
+    pedidoCompra = models.ForeignKey('PedidoCompra', related_name='faturas', null=False, blank=False)
+
+    class Meta:
+        verbose_name = 'Fatura Recebimento'
+        verbose_name_plural = 'Faturas Recebimento'
