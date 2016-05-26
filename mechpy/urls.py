a11648b334d7328e1f0468998f05101d64420479
Horácio Dias <horacio.dias92@gmail.com>
2016-05-25 22:45:08 -0300

    adicionando foundation

--- mechpy/urls.py
+++ mechpy/urls.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 from rest_framework.authtoken.views import obtain_auth_token
 from mechpy.conta.urls import router as conta_router
 
+from django.views.generic import TemplateView
+
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^api/token/', obtain_auth_token, name='api-token'),
     url(r'^api/', include(conta_router.urls)),
     #url(r'^api/auth/', include('rest_framework.urls', namespace='rest_framework')),
